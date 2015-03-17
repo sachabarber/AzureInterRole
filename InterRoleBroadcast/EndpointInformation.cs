d0e5a774381cb4726d462437a378fb831c8fd0ba
unknown <sacha.barber@gmail.com>
2015-03-17 21:42:42 +0000

    modified files

--- InterRoleBroadcast/EndpointInformation.cs
+++ InterRoleBroadcast/EndpointInformation.cs
@@ -6,12 +6,8 @@ namespace InterRoleBroadcast
         public const string ServiceNamespace = "sbinterroletest";
         public const string ServicePath = "BroadcastService";
         public const string IssuerName = "RootManageSharedAccessKey";
-        public const string IssuerSecret = "zflQOE5zlRwwk5+nPcJ6ghE/nInW0Ts0jogyHIy1a40=";
+        public const string IssuerSecret = "KeyHere";
         public const string KeyName = "RootManageSharedAccessKey";
-        public const string Key = "zflQOE5zlRwwk5+nPcJ6ghE/nInW0Ts0jogyHIy1a40=";
-        //public const string ServiceNamespace = "CHANGE THIS TO YOUR NAMESPACE";
-        //public const string ServicePath = "BroadcastService";
-        //public const string IssuerName = "CHANGE THIS TO YOUR ISSUER NAME";
-        //public const string IssuerSecret = "CHANGE THIS TO YOUR ISSUER SECRET";
+        public const string Key = "KeyHere";
     }
 }
